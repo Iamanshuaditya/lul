5127e071aaec0e42a4422723fcad1ff7823e3172
Anshu Aditya <anshuaditya825301@gmail.com>
2024-04-24 17:45:50 +0530

    navigation for logo updated

--- app/dashboard/_components/logo.tsx
+++ app/dashboard/_components/logo.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
-  return <Image height={130} width={130} alt="Logo" src="/logo.svg" />;
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push("/dashboard")}
+      height={130}
+      width={130}
+      alt="Logo"
+      src="/logo.svg"
+    />
+  );
 };
 
 export default Logo;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -26,7 +26,7 @@ const NavbarRoutes = () => {
       )}
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isCoursePage ? (
-          <Link href="/">
+          <Link href="/dashboard">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
               Exit
